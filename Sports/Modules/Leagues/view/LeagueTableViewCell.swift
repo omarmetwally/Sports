75b9d01692df7caa6ad5a2285fe8c78287776b71
omarmetwally <omamretwally2008@hotmail.com>
2024-05-10 22:35:15 +0300

    Leagues UI and logic "fetching data" from the API

---
+++ Sports/Modules/Leagues/view/LeagueTableViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  LeagueTableViewCell.swift
+//  Sports
+//
+//  Created by Omar on 10/05/2024.
+//
+import UIKit
+import Kingfisher
+
+class LeagueTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var leagueLogoImageView: UIImageView!
+    @IBOutlet weak var leagueNameLabel: UILabel!
+    @IBOutlet weak var countryNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupCardStyle()
+
+    }
+    
+    func configure(with league: League) {
+        let processor = DownsamplingImageProcessor(size: leagueLogoImageView.bounds.size)
+        |> RoundCornerImageProcessor(cornerRadius: 50)
+        leagueNameLabel.text = league.leagueName
+        //        countryNameLabel.text = league.countryName
+        
+        leagueLogoImageView.kf.indicatorType = .activity
+        leagueLogoImageView.kf.setImage(
+            with: league.displayedLeagueLogo,
+            placeholder: UIImage(named: "placeholderImage"),
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(1)),
+                .cacheOriginalImage
+            ])
+    }
+    private func setupCardStyle() {
+        contentView.backgroundColor = UIColor.lightGray
+        contentView.layer.cornerRadius = 10
+        contentView.layer.masksToBounds = true
+        
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 8)
+        layer.shadowRadius = 4
+        layer.shadowOpacity = 0.8
+        layer.masksToBounds = false
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: contentView.layer.cornerRadius).cgPath
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let cellSpacing: CGFloat = 10
+        let horizontalMargin: CGFloat = 20
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: cellSpacing / 2, left: horizontalMargin, bottom: cellSpacing / 2, right: horizontalMargin))
+        
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: contentView.layer.cornerRadius).cgPath
+    }
+    
+    
+
+}
+
