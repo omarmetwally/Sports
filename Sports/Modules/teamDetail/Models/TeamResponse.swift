f1817137b1f114b99e65f191977f99eed15e7b48
A0Ghoneim <ahmeduuugogo3@gmail.com>
2024-05-11 11:52:38 +0200

    adding and ui Team Details screen along with network call

---
+++ Sports/Modules/teamDetail/Models/TeamResponse.swift
@@ -0,0 +1,68 @@
+//
+//  Team.swift
+//  Sports
+//
+//  Created by user242921 on 5/11/24.
+//
+
+import Foundation
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let team = try? JSONDecoder().decode(Team.self, from: jsonData)
+
+import Foundation
+
+// MARK: - Team
+struct TeamResponse: Codable {
+    let success: Int
+    let result: [TeamResult]
+}
+
+// MARK: - Result
+struct TeamResult: Codable {
+    let teamKey: Int
+    let teamName: String
+    let teamLogo: String
+    let players: [Player]
+    let coaches: [Coach]
+
+    enum CodingKeys: String, CodingKey {
+        case teamKey = "team_key"
+        case teamName = "team_name"
+        case teamLogo = "team_logo"
+        case players, coaches
+    }
+}
+
+
+
+
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
+
---
+++ Sports/Modules/teamDetail/view/TeamCell.swift
@@ -0,0 +1,23 @@
+//
+//  TeamCell.swift
+//  Sports
+//
+//  Created by user242921 on 5/11/24.
+//
+
+import UIKit
+
+class TeamCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
