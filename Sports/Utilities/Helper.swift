849f4c00aebd2e7fec1668533ceb846923a63e21
omarmetwally <omamretwally2008@hotmail.com>
2024-05-13 00:19:00 +0300

    Offline Favorite Reachability for connection in app delegate

--- Sports/Utilities/Helper.swift
+++ Sports/Utilities/Helper.swift
@@ -21,22 +21,33 @@ class Helper {
     }
     
     static func presentAddToFavoritesAlert(from viewController: UIViewController, yesAction: @escaping () -> Void) {
-            let alert = UIAlertController(title: nil, message: Constants.Alerts.addFavoriteMessage, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: Constants.Alerts.yesButtonTitle, style: .default, handler: { _ in
-                yesAction()
-            }))
-            alert.addAction(UIAlertAction(title: Constants.Alerts.noButtonTitle, style: .cancel, handler: nil))
-            
-            viewController.present(alert, animated: true)
-        }
+        let alert = UIAlertController(title: nil, message: Constants.Alerts.addFavoriteMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: Constants.Alerts.yesButtonTitle, style: .default, handler: { _ in
+            yesAction()
+        }))
+        alert.addAction(UIAlertAction(title: Constants.Alerts.noButtonTitle, style: .cancel, handler: nil))
         
-        static func presentRemoveFromFavoritesAlert(from viewController: UIViewController, yesAction: @escaping () -> Void) {
-            let alert = UIAlertController(title: nil, message: Constants.Alerts.removeFavoriteMessage, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: Constants.Alerts.deleteButtonTitle, style: .destructive, handler: { _ in
-                yesAction()
-            }))
-            alert.addAction(UIAlertAction(title: Constants.Alerts.noButtonTitle, style: .cancel, handler: nil))
-            
-            viewController.present(alert, animated: true)
-        }
+        viewController.present(alert, animated: true)
+    }
+    
+    static func presentRemoveFromFavoritesAlert(from viewController: UIViewController, yesAction: @escaping () -> Void) {
+        let alert = UIAlertController(title: nil, message: Constants.Alerts.removeFavoriteMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: Constants.Alerts.deleteButtonTitle, style: .destructive, handler: { _ in
+            yesAction()
+        }))
+        alert.addAction(UIAlertAction(title: Constants.Alerts.noButtonTitle, style: .cancel, handler: nil))
+        
+        viewController.present(alert, animated: true)
+    }
+    
+    static func presentNetworkAlert(from viewController: UIViewController) {
+        let alert = UIAlertController(title: nil, message: Constants.Alerts.networkErrorMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: Constants.Alerts.openSettings, style: .default, handler: { _ in
+            if let url = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }))
+        alert.addAction(UIAlertAction(title: Constants.Alerts.cancel, style: .cancel, handler: nil))
+        viewController.present(alert, animated: true, completion: nil)
+    }
 }
